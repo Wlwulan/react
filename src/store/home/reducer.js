1258ff6b760d7c17edd21e0709cbedf87af5a781
吴兰 <569175240@qq.com>
2019-03-08 18:29:25 +0800

    react项目未完善

---
+++ src/store/home/reducer.js
@@ -0,0 +1,23 @@
+import * as home from './action-type';
+
+let defaultState = {
+  orderSum: '', //金额
+  name: '', //姓名
+  phoneNo: '', //手机号
+  imgpath: '', //图片地址
+}
+// 首页表单数据
+export const formData = (state = defaultState , action = {}) => {
+  switch(action.type){
+    case home.SAVEFORMDATA:
+      return {...state, ...{[action.datatype]: action.value}};
+    case home.SAVEIMG:
+      console.log(111,...state)
+      return {...state, ...{imgpath: action.path}};
+    case home.CLEARDATA:
+      return {...state, ...defaultState};
+    default:
+      return state;
+  }
+}
+
